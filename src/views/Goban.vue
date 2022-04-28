79b269e0eacbdf18f8b36b81af16951e7c75f757
2alheure <jojo77.juv@live.fr>
2022-04-28 23:14:09 +0200

    Playable stones

--- src/views/Goban.vue
+++ src/views/Goban.vue
@@ -1,13 +1,32 @@
 <script setup>
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 
 const size = ref(9);
 const whiteTurn = ref(true);
-// const isKo = ref(false);
-const isKo = ref({
-  x: 9,
-  y: 6
+const isKo = ref(false);
+// const isKo = ref({
+//   x: 9,
+//   y: 6,
+// });
+const map = ref({});
+
+onMounted(() => {
+  for (var i = 1; i <= size.value; i++) {
+    var array = {};
+    for (var j = 1; j <= size.value; j++) {
+      array[j] = undefined;
+    }
+    map.value[i] = array;
+  }
 });
+
+const play = (x, y) => {
+  if (map.value[x][y] !== undefined || (isKo && isKo.x == x && isKo.y == y)) return;
+  console.log('play', x, y);
+
+  map.value[x][y] = whiteTurn.value;
+  whiteTurn.value = !whiteTurn.value;
+};
 </script>
 
 <template>
@@ -19,15 +38,16 @@ const isKo = ref({
         :id="x + '-' + y"
         :class="{
           case: true,
-          top: x == 1,
-          bottom: x == size,
-          right: y == size,
-          left: y == 1,
+          haut: x == 1,
+          bas: x == size,
+          droite: y == size,
+          gauche: y == 1,
           ko: isKo && isKo.x == x && isKo.y == y,
-          noir: true
+          blanc: map[x][y] !== undefined && map[x][y] === true,
+          noir: map[x][y] !== undefined && map[x][y] === false,
         }"
       >
-        <div class="pierre"></div>
+        <div class="pierre" @click="play(x, y)"></div>
       </div>
     </template>
   </main>
@@ -39,7 +59,7 @@ main {
   display: grid;
   grid-template-rows: repeat(9, 1fr);
   grid-template-columns: repeat(9, 1fr);
-  background-color: orange;
+  background-image: url("https://raw.githubusercontent.com/atarnowsky/go-assets/master/images/board/wood_full_lighter.jpg");
 }
 
 @media (orientation: landscape) {
@@ -62,6 +82,7 @@ main .case {
   display: flex;
   justify-content: center;
   align-items: center;
+  border: 1px solid black;
 }
 
 main .case .pierre {
@@ -78,8 +99,13 @@ main .case.blanc .pierre {
   background-color: #eee;
 }
 
+main .case:not(.blanc, .noir) .pierre:hover {
+  border: 3px solid rgba(255, 255, 255, 0.5);
+  cursor: pointer;
+}
+
 main .case.ko ::before {
-  content: '⏳';
+  content: "⏳";
   font-size: 30px;
   font-weight: 900;
   width: 100%;
